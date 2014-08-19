7753e63e8ad8679f21b9f81ad4d1e4c7b769c3f1
Athlan <athlan@vgroup.pl>
2014-08-20 01:31:05 +0200

    Try load image.

--- DumpPlotDataFromBmp/src/main/java/DumpPlotDataFromBmp.java
+++ DumpPlotDataFromBmp/src/main/java/DumpPlotDataFromBmp.java
@@ -1,8 +1,21 @@
 //package pl.polsl;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
 public class DumpPlotDataFromBmp {
 	
 	public static void main(String[] argv) {
-		System.out.println("Hello!");
+		
+		if(argv.length != 1) {
+			throw new RuntimeException("Invalid numer of parameters. Excepted only one.");
+		}
+		
+		String filePath = argv[0];
+		
+		BufferedImage img = ImageIO.load(filePath);
+		int[] pixels = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
+		
+		System.out.println("Hello! Done.");
 	}
 }
