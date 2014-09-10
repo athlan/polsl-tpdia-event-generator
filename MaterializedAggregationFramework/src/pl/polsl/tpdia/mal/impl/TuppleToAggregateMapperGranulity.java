5e54d687b7f2461222625cb69e102abcee79c6d6
Athlan <athlan@vgroup.pl>
2014-09-10 03:58:14 +0200

    Fixes. Mapper and Reducer explained.

--- MaterializedAggregationFramework/src/pl/polsl/tpdia/mal/impl/TuppleToAggregateMapperGranulity.java
+++ MaterializedAggregationFramework/src/pl/polsl/tpdia/mal/impl/TuppleToAggregateMapperGranulity.java
@@ -14,17 +14,7 @@ public enum TuppleToAggregateMapperGranulity {
 	},
 	DAY {
 		public int getAlignedTimestamp(int timestamp) {
-			return (timestamp / 86400) * 86400; // time aligned to 3600 secs
-		}
-	},
-	MONTH {
-		public int getAlignedTimestamp(int timestamp) {
-			return 0;
-		}
-	},
-	YEAR {
-		public int getAlignedTimestamp(int timestamp) {
-			return 0;
+			return (timestamp / 86400) * 86400; // time aligned to 86400 secs
 		}
 	};
 	
